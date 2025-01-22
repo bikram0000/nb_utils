be1055d3835a1f52665ffe87db34ac6b74dbc71b
Bikramaditya meher <43512518+bikram0000@users.noreply.github.com>
2025-01-22 22:53:40 +0530

    Update shared_pref.dart
    used getstorage on this file

--- lib/src/utils/shared_pref.dart
+++ lib/src/utils/shared_pref.dart
@@ -1,189 +1,175 @@
 import 'dart:convert';
 
 import 'package:nb_utils/nb_utils.dart';
+import 'package:get_storage/get_storage.dart';
 
-/// Returns SharedPref Instance
-Future<SharedPreferences> getSharedPref() async {
-  return await SharedPreferences.getInstance();
+final GetStorage storage = GetStorage();
+
+/// Initialize GetStorage
+Future<void> initStorage() async {
+  await GetStorage.init();
 }
 
-/// Add a value in SharedPref based on their type - Must be a String, int, bool, double, Map<String, dynamic> or StringList
+/// Add a value to GetStorage based on its type (supports String, int, bool, double, Map<String, dynamic>, StringList)
 Future<bool> setValue(String key, dynamic value, {bool print = true}) async {
   if (value == null) {
-    if (print) log('$key - value is null');
+    if (print) print('$key - value is null');
     return Future.value(false);
   }
-  if (print) log('${value.runtimeType} - $key - $value');
+  if (print) print('${value.runtimeType} - $key - $value');
 
   if (value is String) {
-    return await sharedPreferences.setString(key, value.validate());
+    storage.write(key, value);
   } else if (value is int) {
-    return await sharedPreferences.setInt(key, value.validate());
+    storage.write(key, value);
   } else if (value is bool) {
-    return await sharedPreferences.setBool(key, value.validate());
+    storage.write(key, value);
   } else if (value is double) {
-    return await sharedPreferences.setDouble(key, value.validate());
+    storage.write(key, value);
   } else if (value is Map<String, dynamic>) {
-    return await sharedPreferences.setString(key, jsonEncode(value));
+    storage.write(key, jsonEncode(value));
   } else if (value is List<String>) {
-    return await sharedPreferences.setStringList(key, value);
+    storage.write(key, value);
   } else {
     throw ArgumentError(
-        'Invalid value ${value.runtimeType} - Must be a String, int, bool, double, Map<String, dynamic> or StringList');
+      'Invalid value ${value.runtimeType} - Must be a String, int, bool, double, Map<String, dynamic> or StringList');
   }
+  return true;
 }
 
-/// Returns List of Keys that matches with given Key
-List<String> getMatchingSharedPrefKeys(String key) {
+/// Returns List of Keys that match the given key (GetStorage does not have a direct equivalent for this)
+List<String> getMatchingStorageKeys(String key) {
   List<String> keys = [];
-
-  sharedPreferences.getKeys().forEach((element) {
+  // In GetStorage, you would need to manually check keys, as GetStorage does not have `getKeys` method
+  storage.getKeys().forEach((element) {
     if (element.contains(key)) {
       keys.add(element);
     }
   });
-
   return keys;
 }
 
-/// Returns a StringList if exists in SharedPref
+/// Returns a StringList if exists in GetStorage
 List<String>? getStringListAsync(String key) {
-  return sharedPreferences.getStringList(key);
+  return storage.read<List<String>>(key);
 }
 
-/// Returns a Bool if exists in SharedPref
+/// Returns a Bool if exists in GetStorage
 bool getBoolAsync(String key, {bool defaultValue = false}) {
-  return sharedPreferences.getBool(key) ?? defaultValue;
+  return storage.read<bool>(key) ?? defaultValue;
 }
 
-/// Returns a Double if exists in SharedPref
+/// Returns a Double if exists in GetStorage
 double getDoubleAsync(String key, {double defaultValue = 0.0}) {
-  return sharedPreferences.getDouble(key) ?? defaultValue;
+  return storage.read<double>(key) ?? defaultValue;
 }
 
-/// Returns a Int if exists in SharedPref
+/// Returns an Int if exists in GetStorage
 int getIntAsync(String key, {int defaultValue = 0}) {
-  return sharedPreferences.getInt(key) ?? defaultValue;
+  return storage.read<int>(key) ?? defaultValue;
 }
 
-/// Returns a String if exists in SharedPref
+/// Returns a String if exists in GetStorage
 String getStringAsync(String key, {String defaultValue = ''}) {
-  return sharedPreferences.getString(key) ?? defaultValue;
+  return storage.read<String>(key) ?? defaultValue;
 }
 
-/// Returns a JSON if exists in SharedPref
-Map<String, dynamic> getJSONAsync(String key,
-    {Map<String, dynamic>? defaultValue}) {
-  if (sharedPreferences.containsKey(key) &&
-      sharedPreferences.getString(key).validate().isNotEmpty) {
-    return jsonDecode(sharedPreferences.getString(key)!);
+/// Returns a JSON (Map<String, dynamic>) if exists in GetStorage
+Map<String, dynamic> getJSONAsync(String key, {Map<String, dynamic>? defaultValue}) {
+  String? jsonString = storage.read<String>(key);
+  if (jsonString != null && jsonString.isNotEmpty) {
+    return jsonDecode(jsonString);
   } else {
     return defaultValue ?? {};
   }
 }
 
-/// remove key from SharedPref
+/// Remove key from GetStorage
 Future<bool> removeKey(String key) async {
-  return await sharedPreferences.remove(key);
+  await storage.remove(key);
+  return true;
 }
 
-/// clear SharedPref
-Future<bool> clearSharedPref() async {
-  return await sharedPreferences.clear();
+/// Clear GetStorage
+Future<bool> clearStorage() async {
+  await storage.erase();
+  return true;
 }
 
 /////////////////////////////////////////////////////////////////////// DEPRECATED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-/// add a Double in SharedPref
+/// Deprecated methods, use `setValue` and `get*Async` methods instead
+
 @Deprecated('Use setValue instead')
 Future<bool> setDoubleAsync(String key, double value) async {
-  return await sharedPreferences.setDouble(key, value);
+  storage.write(key, value);
+  return true;
 }
 
-/// add a Bool in SharedPref
 @Deprecated('Use setValue instead')
 Future<bool> setBoolAsync(String key, bool value) async {
-  return await sharedPreferences.setBool(key, value);
+  storage.write(key, value);
+  return true;
 }
 
-/// add a Int in SharedPref
 @Deprecated('Use setValue instead')
 Future<bool> setIntAsync(String key, int value) async {
-  return await sharedPreferences.setInt(key, value);
+  storage.write(key, value);
+  return true;
 }
 
-/// add a String in SharedPref
 @Deprecated('Use setValue instead')
 Future<bool> setStringAsync(String key, String value) async {
-  return await sharedPreferences.setString(key, value);
+  storage.write(key, value);
+  return true;
 }
 
-/// add a JSON in SharedPref
 @Deprecated('Use setValue instead')
 Future<bool> setJSONAsync(String key, String value) async {
-  return await sharedPreferences.setString(key, jsonEncode(value));
+  storage.write(key, jsonEncode(value));
+  return true;
 }
 
-/// Returns a String if exists in SharedPref
 @Deprecated('Use getStringAsync instead without using await')
 Future<String> getString(String key, {defaultValue = ''}) async {
-  return await getSharedPref().then((pref) {
-    return pref.getString(key) ?? defaultValue;
-  });
+  return storage.read<String>(key) ?? defaultValue;
 }
 
-/// Returns a Int if exists in SharedPref
 @Deprecated('Use getIntAsync instead without using await')
 Future<int> getInt(String key, {defaultValue = 0}) async {
-  return await getSharedPref().then((pref) {
-    return pref.getInt(key) ?? defaultValue;
-  });
+  return storage.read<int>(key) ?? defaultValue;
 }
 
-/// Returns a Double if exists in SharedPref
 @Deprecated('Use getDoubleAsync instead without using await')
 Future<double> getDouble(String key, {defaultValue = 0.0}) async {
-  return await getSharedPref().then((pref) {
-    return pref.getDouble(key) ?? defaultValue;
-  });
+  return storage.read<double>(key) ?? defaultValue;
 }
 
-/// Returns a Bool if exists in SharedPref
 @Deprecated('Use getBoolAsync instead without using await')
 Future<bool> getBool(String key, {defaultValue = false}) async {
-  return await getSharedPref().then((pref) {
-    return pref.getBool(key) ?? defaultValue;
-  });
+  return storage.read<bool>(key) ?? defaultValue;
 }
 
-/// add a String in SharedPref
 @Deprecated('Use setValue instead')
 Future<bool> setString(String key, String value) async {
-  return await getSharedPref().then((pref) async {
-    return await pref.setString(key, value);
-  });
+  storage.write(key, value);
+  return true;
 }
 
-/// add a Int in SharedPref
 @Deprecated('Use setValue instead')
 Future<bool> setInt(String key, int value) async {
-  return await getSharedPref().then((pref) async {
-    return await pref.setInt(key, value);
-  });
+  storage.write(key, value);
+  return true;
 }
 
-/// add a Bool in SharedPref
 @Deprecated('Use setValue instead')
 Future<bool> setBool(String key, bool value) async {
-  return await getSharedPref().then((pref) async {
-    return await pref.setBool(key, value);
-  });
+  storage.write(key, value);
+  return true;
 }
 
-/// add a Double in SharedPref
 @Deprecated('Use setValue instead')
 Future<bool> setDouble(String key, double value) async {
-  return await getSharedPref().then((pref) async {
-    return await pref.setDouble(key, value);
-  });
+  storage.write(key, value);
+  return true;
 }
